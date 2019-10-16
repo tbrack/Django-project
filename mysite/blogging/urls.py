4a32a7b6f8bc5aa854bea5116ec99270d403c594
Travis Brackney <tbrackney@gmail.com>
2019-10-15 20:31:03 -0700

    added list view

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -1,6 +1,7 @@
-from blogging.views import stub_view
+from blogging.views import list_view, stub_view
 from django.urls import path
 
 urlpatterns = [
-    path('', stub_view, name="blog_index"),
+    path('', list_view, name="blog_index"),
+    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
 ]
